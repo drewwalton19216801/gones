6564098ac16a7af04cff4076f96c559e86ab8a53
Drew Walton <drewwalton19216801@gmail.com>
2023-12-15 20:45:16 -0700

    Comment update

--- cpu/bus.go
+++ cpu/bus.go
@@ -1,6 +1,6 @@
 package cpu
 
-// Bus is an interface for interacting with the CPU's memory
+// Bus is an interface for interacting with the memory attached to the CPU
 type Bus interface {
 	Read(addr uint16) byte
 	Write(addr uint16, data byte)
